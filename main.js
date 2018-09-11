02521b9c84bc9608858d69d085f6724eb98e0ed4
Daniel van den Bongard <daniel@vdbongard.com>
2018-09-11 23:34:24 +0200

    Added a consistent application name

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ let mainWindow
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
+    title: 'Photo Viewer',
     width: 800,
     height: 600,
     frame: false,
